d0fe88053ad3f8dac59781d6f62fd6ee0054686e
Lola Galvan <101185678+lolagalvan@users.noreply.github.com>
2023-03-27 11:55:22 -0300

    Add files via upload

--- LaboA2023/src/Main.java
+++ LaboA2023/src/Main.java
@@ -1,25 +1,69 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        /*
-        ejercicio libro
-        Libro libro1 = new Libro("Sombra y hueso");
-        libro1.mostrar(libro1);
-        libro.libroAnterior(libro1);
+            /* ArrayList<Integer> numeros = new ArrayList<Integer>();
+        Scanner entrada = new Scanner(System.in);
+        int cantidad;
+        cantidad = entrada.nextInt();
+
+        for (int i=0; i<cantidad;i++){
+            int num;
+            num = entrada.nextInt();
+            numeros.add(num);
+        }
+        int suma = 0;
+        for (int numero: numeros) {
+            suma = numero + suma;
 
 
-        ejercicio cafetera
-        Cafetera cafetera = new Cafetera();
-        cafetera.vaciascafetera();
-        cafetera.agregacafe(100);
-        cafetera.llenarcafetera();
-        cafetera.servirtaza(500);
+        }
+        System.out.println("la suma es: "+ suma); */
+
+       /*ArrayList<String> palabra = new ArrayList<String>();
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("cantidad: ");
+        int cantidad;
+        cantidad = entrada.nextInt();
+        System.out.println("palabras: ");
+        for (int i=0; i<cantidad;i++){
+
+            String pal;
+            pal = entrada.next();
+            palabra.add(pal);
+        }
+
+        for (String palabras: palabra) {
+            if (palabras.charAt(0)=='l'|| palabras.charAt(0)=='a'){
+                System.out.println(palabras);
+            }
+        }*/
 
 
-        ejercicio cancion
-        Cancion cancion = new Cancion();*/
+        ArrayList<Persona> persona = new ArrayList<Persona>();
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("cantidad: ");
+        int cantidad;
+        cantidad = entrada.nextInt();
 
 
+        for (int i = 0; i < cantidad; i++) {
+            String nombre = entrada.next();
+            int edad = entrada.nextInt();
+            String direccion = entrada.next();
+            Persona persona1 = new Persona(nombre, edad, direccion);
 
+        }
+        for (Persona personas : persona) {
+            if (personas.getEdad() > 30) {
+                System.out.println(personas.imprimir());
+            }
+
+        }
     }
 }
+
+
