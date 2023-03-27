eb98f1ce0d981f68e6cee03e7f64ec16092b3fd4
Lola Galvan <101185678+lolagalvan@users.noreply.github.com>
2023-03-27 08:21:05 -0300

    Add files via upload

---
+++ LaboA2023/src/Cancion.java
@@ -0,0 +1,34 @@
+public class Cancion {
+
+    private String titulo;
+    private Persona autor;
+
+
+    public void Cancion(String titulo, Persona autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    public Cancion() {
+        this.titulo = " ";
+        autor = new Persona();
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Persona getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Persona autor) {
+        this.autor = autor;
+    }
+}
+
+
