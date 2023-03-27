4a328cf64a4539477fba7e516a9850698d409677
Lola Galvan <101185678+lolagalvan@users.noreply.github.com>
2023-03-27 08:30:56 -0300

    Add files via upload

---
+++ LaboA2023/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        ejercicio libro
+        Libro libro1 = new Libro("Sombra y hueso");
+        libro1.mostrar(libro1);
+        libro.libroAnterior(libro1);
+
+
+        ejercicio cafetera
+        Cafetera cafetera = new Cafetera();
+        cafetera.vaciascafetera();
+        cafetera.agregacafe(100);
+        cafetera.llenarcafetera();
+        cafetera.servirtaza(500);
+
+
+        ejercicio cancion
+        Cancion cancion = new Cancion();*/
+
+
+
+    }
+}
