d0e161220962d086a8733ef7facfafa2404bdad1
Lola Galvan <101185678+lolagalvan@users.noreply.github.com>
2023-09-03 11:19:34 -0300

    repo completo

---
+++ LaboA2023/src/Operacion.java
@@ -0,0 +1,21 @@
+public  class Operacion {
+
+public static int suma(int n1,int n2){
+    return n1+n2;
+}
+public static int resta(int n1,int n2){
+        return n1+n2;
+}
+
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("la suma de 2+4 es");
+        System.out.println(Operacion.suma(2,4));
+        System.out.println("la resta de 6-2 es");
+        System.out.println(Operacion.resta(6,2));
+    }
+}
+
